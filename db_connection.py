70f86b7b210cfd368984b9febd9842c14b6d28e1
Zihao C <62597814+czh2152215@users.noreply.github.com>
2023-11-15 01:16:54 -0500

    database

---
+++ db_connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import URL
+
+sql_database_url = URL.create(
+    drivername="mysql",
+    username="admin",
+    password="password!",
+    host="adopter-1.ccixfecy4h7f.us-east-1.rds.amazonaws.com",
+    database="adopter",
+    port=3306
+)
+
+
+engine=create_engine(sql_database_url)
+
+SessionLocal=sessionmaker(autocommit=False,bind=engine)
+
+base=declarative_base()
--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 from typing import Union
 
-import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import uvicorn
+from db_connection import SessionLocal
+from models.Adopter import Adopter
+from fastapi import HTTPException
+
 
 app = FastAPI()
-
-
+session = SessionLocal()
+class AdopterOut(BaseModel):
+    id:int
+    name:str
+    age:int
+    email:str
+    phone:str
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/adopter/{adopter_id}", response_model=AdopterOut)
+def get_adopter(adopter_id: int):
+    try:
+        adopter = session.query(Adopter).filter(Adopter.id == adopter_id).first()
+        if adopter is None:
+            raise HTTPException(status_code=404, detail="Adopter not found")
+        return adopter
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
-@app.get("/users")
-async def users():
-    users = [
-        {
-            "name": "Mars Kule",
-            "age": 25,
-            "city": "Lagos, Nigeria"
-        },
-
-        {
-            "name": "Mercury Lume",
-            "age": 23,
-            "city": "Abuja, Nigeria"
-        },
-
-         {
-            "name": "Jupiter Dume",
-            "age": 30,
-            "city": "Kaduna, Nigeria"
-        }
-    ]
-
-    return users
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8011)
